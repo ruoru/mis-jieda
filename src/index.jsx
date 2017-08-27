87c5f36a34c65e7bd48be82cef18af8c9a477982
ruoru <cruoru@gmail.com>
2017-08-27 22:56:39 +0800

    add redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app.jsx';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './containers/App';
+import todoApp from './reducers';
 
-ReactDOM.render(
-  <App />,
+render(
+  <Provider store={createStore(todoApp)}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
+)
